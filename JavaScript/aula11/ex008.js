26bf236963e981fe646fee45fcf839f145211c8c
Ana Clara <127421985+AnaClara-Amorim@users.noreply.github.com>
2023-04-19 16:06:46 -0300

    Create ex008.js

---
+++ JavaScript/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12 
+console.log(`A velocidade do seu carro é ${vel}Km/s`)
+if (vel > 60) { // Condição simples, não tem else
+    console.log(`Você ultrapassou a velocidade. MULTADO`)
+}
+console.log('Dirija sempre usando cinto de segurança')
