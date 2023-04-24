e47df08147fb2d0e4df130ffe78895d18aaf4220
Ana Clara <127421985+AnaClara-Amorim@users.noreply.github.com>
2023-04-24 19:27:49 -0300

    Update script.js

--- JavaScript/aula12ex/ex015/script.js
+++ JavaScript/aula12ex/ex015/script.js
@@ -7,6 +7,16 @@ function verificar() {
     if (fano.value.length == 0 || fano.value > ano){
         window.alert('Erro. Verifique os dados e tente novamente novamente!')
     } else {
-        window.alert('Tudo ok!')
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        //res.innerHTML = `Idade calculada ${idade}`
+        var genero = ''
+        if (fsex[1].checked) {
+            genero = 'homem'
+        } else if (fsex[0].checked) {
+            genero = 'mulher'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos de idade.`
     }
 }
