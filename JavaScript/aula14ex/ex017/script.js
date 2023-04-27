e3279a4c492f2d554e978d7fce865fa5c99adf5e
Ana Clara <127421985+AnaClara-Amorim@users.noreply.github.com>
2023-04-27 17:50:25 -0300

    update aula14ex

--- JavaScript/aula14ex/ex017/script.js
+++ JavaScript/aula14ex/ex017/script.js
@@ -7,9 +7,13 @@ function tabuada() {
     } else {
         let n = Number(num.value)
         let c = 1
-
-        while (c<=10) {
-            let item = document.createElement('option')
+        tab.innerHTML = '' //Para limpar a tabuada
+        while (c <=10 ) {
+            let item = document.createElement('option') //Foi criado aqui e não no html
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`//Linha para dar valor, faz sentido em outra linguagens
+            tab.appendChild(item)
+            c++
         }
     }
 }
