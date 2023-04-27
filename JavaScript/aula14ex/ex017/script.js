208da82383a7cc1ec6ebfe2cbec69b89b3d4f72c
Ana Clara <127421985+AnaClara-Amorim@users.noreply.github.com>
2023-04-27 17:35:03 -0300

    up

--- JavaScript/aula14ex/ex017/script.js
+++ JavaScript/aula14ex/ex017/script.js
@@ -1,1 +1,10 @@
-alert ('Olá!')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+    }
+}
