746de1647f676fd48d8383339f67b9bd49bccf07
Ana Clara <127421985+AnaClara-Amorim@users.noreply.github.com>
2023-04-28 16:39:38 -0300

    Update ambiente.js

--- JavaScript/aula15/ambiente.js
+++ JavaScript/aula15/ambiente.js
@@ -14,5 +14,15 @@ console.log (`O vetor tem ${num.length} posições`) //Mostra quantos elementos 
 num.sort() // Coloca os valores em ordem crescente
 console.log (`O primeiro valor do vetor é ${num[0]}`) // Mostra o valor no índice 
 
+// PARA MOSTRAR O VETOR SEM A FORMATAÇÃO PADRÃO
 
+for (let pos = 0; pos <num.length; pos++){ 
+    console.log(`A posição ${pos} tem valor ${num[pos]}`)
+}
+
+// JEITO MAIS SIMPLIFICADO, otimizado para arrays
+
+for (let pos in num) { //Para cada posição em num
+    console.log (num[pos])
+}
 
