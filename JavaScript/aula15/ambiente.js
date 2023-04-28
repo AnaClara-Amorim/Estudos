7c41a474c277f993c8094fe16b0014ad611abb77
Ana Clara <127421985+AnaClara-Amorim@users.noreply.github.com>
2023-04-28 16:12:25 -0300

    Update ambiente.js

--- JavaScript/aula15/ambiente.js
+++ JavaScript/aula15/ambiente.js
@@ -1,3 +1,16 @@
-let num = [5, 8, 2, 9, 3]
+let num = [5, 8, 2, 9, 3] // Vetor criado
 
-console.log(`Nosso vetor é o ${num}`)
+console.log(`Nosso vetor é o ${num}`) // Mostrando o vetor
+
+num[5] = 6 // Adicionando o valor 6 na posição de índice 5, lembrando que o índice começa em zero
+
+console.log(num) // Mostrando o vator com o novo elemento
+
+num.push(7) // Este comando adiciona o elemento no final do vetor 
+
+console.log(num) 
+
+console.log (`O vetor tem ${num.length} posições`) //Mostra quantos elementos tem
+
+console.log (num[0]) // Mostra o valor no índice 
+
