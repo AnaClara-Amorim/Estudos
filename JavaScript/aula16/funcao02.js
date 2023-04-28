79d5363d3148225d798334bfeefbba7ed25cef33
Ana Clara <127421985+AnaClara-Amorim@users.noreply.github.com>
2023-04-28 17:35:17 -0300

    Update funcao02.js

--- JavaScript/aula16/funcao02.js
+++ JavaScript/aula16/funcao02.js
@@ -1,6 +1,6 @@
-function soma(n1, n2) {
+function soma(n1=6, n2=8) {
     return n1 + n2
 }
 
-//let res = soma(2, 4) pode escrever direto do jeito abaixo
-console.log(soma(2, 4))
+//let res = soma(2, 4) pode escrever direto do jeito abaixo. Se enviar somente um valor nesse caso, dá NaN. Se os valores forem definidos antes, como é o caso, ele vai somar o que tiver
+console.log(soma(4)) 
