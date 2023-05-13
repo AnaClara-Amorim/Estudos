18bdd11e4a9d5a9001475cb810c39374757f9555
Ana Clara <127421985+AnaClara-Amorim@users.noreply.github.com>
2023-05-13 20:54:04 -0300

    up

---
+++ Projeto-Pokemon/src/js/index.js
@@ -0,0 +1,15 @@
+/* Objetivo 1 - Ao clicar no botão de troca de tema, adicionar a classe modo-escuro no body para que os estilos do modo escujo sejam aplicados e mudar a imagem pra lua
+
+- passo 1 - pegar no js o elemento html correspondente  ao botao de troca de tema
+- passo 2 - pegar no js o elemento html correspondente ao body 
+- passo 3 - identificar o clique do usuário no botão de troca de tema
+- passo 4 - adicionar a classe modo escuro no body 
+- passo 5 - trocar a imagem do botão de alterar tema para lua 
+
+Objetivo 2 - quando clicar no botão de trocad de tema, caso o body ja tenha a classe modo-escuro, remover a classe para mudar para modo claro e mudar a imagem para sol
+- passo 6 - remover a classee do modo escuro do body 
+- passo 7 - trocar a imagem do botão de alterar tema para sol */
+
+// passo 1 - pegar no js o elemento html correspondente  ao botao de troca de tema
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
