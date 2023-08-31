442b34518806bfb8338517179fe91b30d7463fb6
Ana Clara <anaamorimsantos1@gmail.com>
2023-08-31 12:20:04 -0300

    Create SequenciaEscape.java

---
+++ Curso-Java-Basico-Loiane/CursoBasico/src/com/loiane/cursoJava/aula11/SequenciaEscape.java
@@ -0,0 +1,14 @@
+package com.loiane.cursoJava.aula11;
+
+public class SequenciaEscape {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//"Hello, world!"
+		
+		System.out.print("\"Hello, world!\"\n\r");
+
+	}
+
+}
