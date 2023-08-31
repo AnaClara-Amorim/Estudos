3ce278a98e527854fcea054cbff952334b089f5f
Ana Clara <anaamorimsantos1@gmail.com>
2023-08-31 12:04:11 -0300

    Adição de mais classes

--- Curso-Java-Basico-Loiane/CursoBasico/src/com/loiane/cursoJava/aula11/VariaveisPontoFlutuante.java
+++ Curso-Java-Basico-Loiane/CursoBasico/src/com/loiane/cursoJava/aula11/VariaveisPontoFlutuante.java
@@ -7,9 +7,11 @@ public class VariaveisPontoFlutuante {
 
 		double valorPassagem = 6.90;
 		float valorTomate = 10.99f; // Sem o f ou F dá erro
+		double d2 = 1.234e2; // O double permite usar notação científica
 		
 		System.out.println("Valor da passagem: " + valorPassagem);
 		System.out.println("Valor do tomate: " + valorTomate);
+		System.out.println("Double com notação científica: " + d2);
 		
 	}
 
