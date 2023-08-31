0eae2d37574279f2c15a1f5c4458dd4cafaf5e4f
Ana Clara <anaamorimsantos1@gmail.com>
2023-08-31 11:47:43 -0300

    Criação de pasta de Curso de Java Basico

---
+++ Curso-Java-Basico-Loiane/CursoBasico/src/com/loiane/cursoJava/aula10/Variaveis.java
@@ -0,0 +1,26 @@
+package com.loiane.cursoJava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Boa prática
+		int idade = 2; 
+		String nome = "Ana";
+		String nomeDaMinhaGata = "Sky";
+		
+		idade = 26; // Reatribuição da variável
+		
+		System.out.println("Idade: " + idade);
+		System.out.println("Nome: " + nome);
+		
+		//Má prática
+		int a = 10;
+		String b = "Ana";
+		
+		
+
+	}
+
+}
---
+++ Curso-Java-Basico-Loiane/CursoBasico/src/com/loiane/cursoJava/aula11/VariaveisPontoFlutuante.java
@@ -0,0 +1,16 @@
+package com.loiane.cursoJava.aula11;
+
+public class VariaveisPontoFlutuante {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double valorPassagem = 6.90;
+		float valorTomate = 10.99f; // Sem o f ou F dá erro
+		
+		System.out.println("Valor da passagem: " + valorPassagem);
+		System.out.println("Valor do tomate: " + valorTomate);
+		
+	}
+
+}
