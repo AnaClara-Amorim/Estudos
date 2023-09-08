ff56947c45c19fac1da7879d6cc0864fafd39740
Ana Clara <anaamorimsantos1@gmail.com>
2023-09-08 17:19:19 -0300

    break e continue

--- Curso-Java-Basico-Loiane/CursoBasico/src/com/loiane/cursoJava/aula16/For.java
+++ Curso-Java-Basico-Loiane/CursoBasico/src/com/loiane/cursoJava/aula16/For.java
@@ -4,6 +4,10 @@ public class For {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("I tem valor: " + i);
+		}
 
 	}
 
