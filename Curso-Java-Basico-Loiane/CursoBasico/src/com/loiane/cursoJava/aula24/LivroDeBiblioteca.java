4b260726491e677340fb2169b225b0dd5ba19f17
Ana Clara <anaamorimsantos1@gmail.com>
2023-09-12 16:31:47 -0300

    Correção de exercícios

--- Curso-Java-Basico-Loiane/CursoBasico/src/com/loiane/cursoJava/aula24/LivroDeBiblioteca.java
+++ Curso-Java-Basico-Loiane/CursoBasico/src/com/loiane/cursoJava/aula24/LivroDeBiblioteca.java
@@ -1,5 +1,16 @@
 package com.loiane.cursoJava.aula24;
 
+import java.util.Date;
+
 public class LivroDeBiblioteca {
 
+	String nome;
+	String autor;
+	int qtdPaginas;
+	int anoLancamento;
+	String isbn;
+	boolean emprestado;
+	Date dataEntrega;
+	String emprestadoA;
+	
 }
--- Curso-Java-Basico-Loiane/CursoBasico/src/com/loiane/cursoJava/aula24/TesteLivro.java
+++ Curso-Java-Basico-Loiane/CursoBasico/src/com/loiane/cursoJava/aula24/TesteLivro.java
@@ -1,10 +1,17 @@
 package com.loiane.cursoJava.aula24;
 
+import java.util.Date;
+
 public class TesteLivro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LivroDeBiblioteca livro = new LivroDeBiblioteca();
+		
+		livro.anoLancamento = 2023;
+		livro.autor = "Loiane";
+		livro.emprestado = true;
+		livro.dataEntrega = new Date(); // Dando a data normal
+		
 	}
 
 }
