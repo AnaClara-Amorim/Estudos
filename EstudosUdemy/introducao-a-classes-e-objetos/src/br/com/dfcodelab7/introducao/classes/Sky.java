32cf47adfb2a3b0f3bea5c67460d8c2c6e9c4ed1
Ana Clara <anaamorimsantos1@gmail.com>
2024-01-15 17:24:28 -0300

    Update Sky.java

--- EstudosUdemy/introducao-a-classes-e-objetos/src/br/com/dfcodelab7/introducao/classes/Sky.java
+++ EstudosUdemy/introducao-a-classes-e-objetos/src/br/com/dfcodelab7/introducao/classes/Sky.java
@@ -1,5 +1,24 @@
 package br.com.dfcodelab7.introducao.classes;
 
 public class Sky {
+	
+	public String especie;
+	public int idade;
+	public int peso;
+	public String tutor;
+	
+	public Sky() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Sky [especie=" + especie + ", idade=" + idade + ", peso=" + peso + ", tutor=" + tutor + "]";
+	}
+	
+	public void miar() {
+		System.out.println("MIAAAAAAAAAAAAAAAU!");
+	}
+	
 
 }
