44f4b349f78db8740a6100c2edd41de8d9298d48
Ana Clara <anaamorimsantos1@gmail.com>
2024-03-19 09:47:08 -0300

    aula

---
+++ ADS/Funtamentos de Desenvolvimento de Software/script.js
@@ -0,0 +1,15 @@
+/*let nome = "Ana ";
+const sobrenome = "Amorim";
+nome1 = "Clara";
+
+alert (nome);
+alert(nome + sobrenome);
+
+let mensagem = `Olá, ${nome}`
+alert(mensagem)
+
+if (idade >= 18) {
+    console("Seja bem-vindo")
+} else {
+    console.log("Acesso bloqueado")
+}*/
