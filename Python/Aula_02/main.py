89ca1de7481be37d3b5adaa7a4b6a22884019617
Ana Clara <anaamorimsantos1@gmail.com>
2024-04-22 16:52:16 -0300

    python

---
+++ Python/Aula_02/main.py
@@ -0,0 +1,11 @@
+#print (1 + 2 + 3 + 4 + 5)
+
+#print ((23 + 19 + 31) /3)
+
+#print (403//73) divisão apenas com o numero inteiro
+
+#print (403%5)
+
+#print (abs(54-44)) valor absoluto
+
+#12:49 min
